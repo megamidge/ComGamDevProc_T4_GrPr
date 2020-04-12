4de6b5b4d82012bc9e1bce84584f4acdaae4916b
ss.samsmith98 <ss.samsmith98@outlook.com>
2020-04-12 16:01:06 +0000

    Ragnar's initial code.

---
+++ Proj/Assets/clickmove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickmove : MonoBehaviour
+{
+    public GameObject[] waypoint;
+    int current = 0;
+    float rotspeed;
+    public float speed;
+    float wpradius = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void move()
+    {
+        
+            if (Vector3.Distance(waypoint[current].transform.position, transform.position) < wpradius)
+            {
+                current++;
+                if (current >= waypoint.Length)
+                {
+                    current = 0;
+                }
+            }
+            transform.position = Vector3.MoveTowards(transform.position, waypoint[current].transform.position, Time.deltaTime * speed);
+        
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+            move();
+            
+       // }
+ 
+    
+    }
+}
